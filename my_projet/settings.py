c2eb391bc748ba44ac4802ed83bc43577260093d
Teo Farias <teofariasfl@gmail.com>
2026-02-23 17:12:20 -0300

    Another bug fixed in paths

--- my_projet/settings.py
+++ my_projet/settings.py
@@ -122,7 +122,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIR = [
+STATICFILES_DIRS = [
     BASE_DIR / "my_app" / "static"
 ]
 
