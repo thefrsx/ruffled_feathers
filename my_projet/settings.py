2d5d65d30d7206fbb37eb94fc5ed44ce358d3947
Teo Farias <teofariasfl@gmail.com>
2026-02-23 17:08:05 -0300

    modified paths for static files

--- my_projet/settings.py
+++ my_projet/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIR = [
     BASE_DIR / "my_app" / "static"
 ]
@@ -129,4 +131,4 @@ LOGOUT_REDIRECT_URL = '/'
 SESSION_COOKIE_AGE = 2 * 60 * 60   # 7200 seconds
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.joim(BASE_DIR, 'media')
